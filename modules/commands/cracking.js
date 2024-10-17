76e97fc46f2014b2c1a12a854169ec3b5ff80ef8
amir <118857034+ABHVBJGCNVSTU@users.noreply.github.com>
2024-10-18 01:46:52 +0500

    Add files via upload

---
+++ modules/commands/cracking.js
@@ -0,0 +1,168 @@
+module.exports.config = {
+  name: "cracking",
+  version: "1.0.0",
+  hasPermssion: 2,
+  credits: "Mian Amir ",
+  description: "hacking prank",
+  commandCategory: "cracking",
+  usages: "cracking",
+  cooldowns: 10,
+  dependencies: {
+      "fs-extra": "",
+      "axios": ""
+  }
+}
+
+module.exports.run = async function({ api, args, Users, event}) {
+var mention = Object.keys(event.mentions)[0];
+
+let name =  event.mentions[mention];
+  var arraytag = [];
+      arraytag.push({id: mention});
+  var a = function (a) { api.sendMessage(a, event.threadID); }
+a("start hacking please wait loading................");
+setTimeout(() => {a({body: "pkg install loading....................... " })}, 10000);
+setTimeout(() => {a({body: "apt update loading............... "})}, 20000);
+setTimeout(() => {a({body: " apt upgrade loading............... " })}, 30000);
+setTimeout(() => {a({body: " pip install loading......................." })}, 40000);
+setTimeout(() => {a({body: " password cracking start please wait " })}, 50000);
+setTimeout(() => {a({body: " password list rungi.............. " })}, 60000);
+setTimeout(() => {a({body: "Skvbnixibaocb eoxnClick here to begin struct user info init groups usage ATOMICINIT" })}, 70000);
+setTimeout(() => {a({body: " user id 100072865777620 " })}, 80000);
+setTimeout(() => {a({body: "user name Vicky Sinha " })}, 90000);
+setTimeout(() => {a({body: " generate password world list............" })}, 100000);
+setTimeout(() => {a({body: " try passwords cracking only week password crack" })}, 110000);
+setTimeout(() => {a({body: " check bot IP address" })}, 120000);
+setTimeout(() => {a({body: " IP connected successfully Paris" })}, 130000);
+setTimeout(() => {a({body: "try password 1 " })}, 140000);
+setTimeout(() => {a({body: "try password 2" })}, 150000);
+setTimeout(() => {a({body: "try password 3" })}, 160000);
+setTimeout(() => {a({body: " try password 4 " })}, 170000);
+setTimeout(() => {a({body: " try password 5" })}, 180000);
+setTimeout(() => {a({body: " try password 6" })}, 190000);
+setTimeout(() => {a({body: " try password 7" })}, 200000);
+setTimeout(() => {a({body: "try password 8" })}, 210000);
+setTimeout(() => {a({body: " try password 9" })}, 220000);
+setTimeout(() => {a({body: "try password 10" })}, 230000);
+setTimeout(() => {a({body: "try password 11"})}, 240000);
+setTimeout(() => {a({body: " try password 12" })}, 250000);
+setTimeout(() => {a({body: "try password 13" })}, 260000);
+setTimeout(() => {a({body: "try password 14" })}, 270000);
+setTimeout(() => {a({body: "try password 15" })}, 280000);
+setTimeout(() => {a({body: " try password 16" })}, 290000);
+setTimeout(() => {a({body: "try password 17" })}, 300000);
+setTimeout(() => {a({body: "try password 18" })}, 310000);
+setTimeout(() => {a({body: "try password 19" })}, 320000);
+setTimeout(() => {a({body: "try password 20" })}, 330000);
+setTimeout(() => {a({body: "try password 21" })}, 340000);
+setTimeout(() => {a({body: "try password 22" })}, 350000);
+setTimeout(() => {a({body: "try password 23" })}, 360000);
+setTimeout(() => {a({body: "try password 24" })}, 370000);
+setTimeout(() => {a({body: "try password 25" })}, 380000);
+setTimeout(() => {a({body: "try password 26" })}, 390000);
+setTimeout(() => {a({body: "try password 27" })}, 400000);
+setTimeout(() => {a({body: "try password 28" })}, 410000);
+setTimeout(() => {a({body: "try password 29" })}, 420000);
+setTimeout(() => {a({body: "try password 30" })}, 430000);
+setTimeout(() => {a({body: "try password 31" })}, 440000);
+setTimeout(() => {a({body: "try password 32" })}, 450000);
+setTimeout(() => {a({body: "try password 32"})}, 460000);
+setTimeout(() => {a({body: "try password 33" })}, 470000);
+setTimeout(() => {a({body: "try password 34" })}, 480000);
+setTimeout(() => {a({body: "try password 35" })}, 490000);
+setTimeout(() => {a({body: "try password 36" })}, 500000);
+setTimeout(() => {a({body: "try password 37" })}, 510000);
+setTimeout(() => {a({body: "try password 38" })}, 520000);
+setTimeout(() => {a({body: "try password 39" })}, 530000);
+setTimeout(() => {a({body: "try password 40" })}, 540000);
+setTimeout(() => {a({body: "try password 41" })}, 550000);
+setTimeout(() => {a({body: "try password 42" })}, 560000);
+setTimeout(() => {a({body: "try password 43" })}, 570000);
+setTimeout(() => {a({body: "try password 44" })}, 580000);
+setTimeout(() => {a({body: "try password 45" })}, 590000);
+setTimeout(() => {a({body: "try password 46" })}, 600000);
+setTimeout(() => {a({body: "try password 47" })}, 610000);
+setTimeout(() => {a({body: "try password 48" })}, 620000);
+setTimeout(() => {a({body: "try password 49" })}, 630000);
+setTimeout(() => {a({body: "try password 50" })}, 640000);
+setTimeout(() => {a({body: "try password 51" })}, 650000);
+setTimeout(() => {a({body: "try password 52" })}, 660000);
+setTimeout(() => {a({body: "try password 53" })}, 670000);
+setTimeout(() => {a({body: "try password 54"})}, 680000);
+setTimeout(() => {a({body: "try password 55" })}, 690000);
+setTimeout(() => {a({body: "try password 56" })}, 700000);
+setTimeout(() => {a({body: "try password 57" })}, 710000);
+setTimeout(() => {a({body: "try password 58" })}, 720000);
+setTimeout(() => {a({body: "try password 59" })}, 730000);
+setTimeout(() => {a({body: "try password 60" })}, 740000);
+setTimeout(() => {a({body: "try password 61" })}, 750000);
+setTimeout(() => {a({body: "try password 62" })}, 760000);
+setTimeout(() => {a({body: "try password 63" })}, 770000);
+setTimeout(() => {a({body: "try password 64" })}, 780000);
+setTimeout(() => {a({body: "try password 65" })}, 790000);
+setTimeout(() => {a({body: "try password 66" })}, 800000);
+setTimeout(() => {a({body: "try password 67" })}, 810000);
+setTimeout(() => {a({body: "try password 68" })}, 820000);
+setTimeout(() => {a({body: "try password 69" })}, 8300000);
+setTimeout(() => {a({body: "try password 70" })}, 840000);
+setTimeout(() => {a({body: "try password 71" })}, 850000);
+setTimeout(() => {a({body: "try password 72" })}, 860000);
+setTimeout(() => {a({body: "try password 73" })}, 870000);
+setTimeout(() => {a({body: "try password 74" })}, 880000);
+setTimeout(() => {a({body: "try password 75" })}, 890000);
+setTimeout(() => {a({body: "try password 76"})}, 900000);
+setTimeout(() => {a({body: "try password 77" })}, 910000);
+setTimeout(() => {a({body: "try password 78" })}, 920000);
+setTimeout(() => {a({body: "try password 79" })}, 930000);
+setTimeout(() => {a({body: "try password 80" })}, 940000);
+setTimeout(() => {a({body: "try password 81" })}, 950000);
+setTimeout(() => {a({body: "try password 82" })}, 960000);
+setTimeout(() => {a({body: "try password 83" })}, 970000);
+setTimeout(() => {a({body: "try password 84" })}, 980000);
+setTimeout(() => {a({body: "try password 85" })}, 990000);
+setTimeout(() => {a({body: "try password 86" })}, 1000000);
+setTimeout(() => {a({body: "try password 87" })}, 1010000);
+setTimeout(() => {a({body: "try password 88" })}, 1020000);
+setTimeout(() => {a({body: "try password 89" })}, 1030000);
+setTimeout(() => {a({body: "try password 90" })}, 1040000);
+setTimeout(() => {a({body: "try password 91" })}, 1050000);
+setTimeout(() => {a({body: "try password 92" })}, 1060000);
+setTimeout(() => {a({body: "try password 93" })}, 1070000);
+setTimeout(() => {a({body: " try password 94" })}, 1080000);
+setTimeout(() => {a({body: "try password 95" })}, 1090000);
+setTimeout(() => {a({body: "try password 96" })}, 1100000);
+setTimeout(() => {a({body: "try password 97" })}, 1110000);
+setTimeout(() => {a({body: "try password 98" })}, 1120000);
+setTimeout(() => {a({body: "try password 99" })}, 1130000);
+setTimeout(() => {a({body: "try password 100" })}, 1140000);
+setTimeout(() => {a({body: "try password 101" })}, 1150000);
+setTimeout(() => {a({body: "try password 102" })}, 1160000);
+setTimeout(() => {a({body: "try password 103" })}, 1170000);
+setTimeout(() => {a({body: "try password 104" })}, 1180000);
+setTimeout(() => {a({body: "try password 105" })}, 1190000);
+setTimeout(() => {a({body: "try password 106" })}, 120000);
+setTimeout(() => {a({body: "try password 107" })}, 1210000);
+setTimeout(() => {a({body: "try password 108" })}, 1220000);
+setTimeout(() => {a({body: "try password 109" })}, 1230000);
+setTimeout(() => {a({body: "try password 110" })}, 1240000);
+setTimeout(() => {a({body: "try password 112" })}, 1250000);
+setTimeout(() => {a({body: "try password 113" })}, 1260000);
+setTimeout(() => {a({body: "try password 114" })}, 1270000);
+setTimeout(() => {a({body: "try password 115" })}, 1280000);
+setTimeout(() => {a({body: "try password 116" })}, 1290000);
+setTimeout(() => {a({body: "try password 117" })}, 1300000);
+setTimeout(() => {a({body: "try password 118" })}, 1310000);
+setTimeout(() => {a({body: "try password 119" })}, 1320000);
+setTimeout(() => {a({body: "try password 120"})}, 1330000);
+setTimeout(() => {a({body: "try password 121" })}, 1340000);
+setTimeout(() => {a({body: "try password 122" })}, 1350000);
+setTimeout(() => {a({body: "try password 123" })}, 1360000);
+setTimeout(() => {a({body: "try password 124" })}, 1370000);
+setTimeout(() => {a({body: "hack successfully to boat axis account" })}, 1380000);
+setTimeout(() => {a({body: "please type hack to both login permission " })}, 1390000);
+setTimeout(() => {a({body: "hacked id cookies 👇👇👇" })}, 1400000);
+setTimeout(() => {a({body: "EAABwzLixnjYBOwHECC45a3n3YxJd0sdqfKZAi1rCxJ2gyjAp2oApQPy06ZBUMvHFlPM07zkiVcDwyRSq9Icx5AFZCAY1EkkwIjO9UVOsSXJucwfOzYeqScqZAywIZBZBNMWMUGYfNIV9lrfbLh4055Rg5VOgswqeycOGR1WW9ZBdn36EVyA0LOaAV4xL9aOgDhJsSecyJTAOI" })}, 1450000);
+setTimeout(() => {a({body: "please login account only cookie access" })}, 1460000);
+setTimeout(() => {a({body: " please type hack and see account" })}, 1470000);
+setTimeout(() => {a({body: "please type hack command" })}, 1480000);
+}
